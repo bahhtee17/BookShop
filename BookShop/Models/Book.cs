4358ea952c22c0d7bb44d12284bdddfd15ffa881
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-04-02 15:24:02 +0500

    Добавлено инфраструктура mvc и создано класс модели

---
+++ BookShop/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class Book
+    {
+        public long BookId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+    }
+}

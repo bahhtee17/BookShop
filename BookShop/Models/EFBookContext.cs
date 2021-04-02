d398d88e84aef13021bf72e3d29508c127c92779
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-04-02 17:16:52 +0500

    Создан класс контекста и класс хранилища

---
+++ BookShop/Models/EFBookContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class EFBookContext : DbContext
+    {
+        public EFBookContext(DbContextOptions<EFBookContext> options): base(options) { }
+        public DbSet<Book> Books { get; set; }
+
+        
+    }
+}
---
+++ BookShop/Models/EFBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class EFBookRepository : IBookRepository
+    {
+        private EFBookContext context;
+        public EFBookRepository(EFBookContext cont)
+        {
+            context = cont;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
